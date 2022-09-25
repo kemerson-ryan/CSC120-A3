ad702829dc8a65667e86a6e83266c07faf2fb908
kemerson-ryan <113268644+kemerson-ryan@users.noreply.github.com>
2022-09-25 13:58:27 -0400

    making changes

--- Conversation.java
+++ Conversation.java
@@ -1,104 +1,78 @@
 import java.util.*;
+import java.util.Arrays;
 
 class Conversation {
+    public static void main(String[] arguments){
+        //Setting up arrays to use within conversation
 
-  public static void main(String[] arguments) {
-    //Setting up arrays to use later
+        //? How to make prior replacements not be overrridden by later ones
+        // Makeshift dictionary to replace required words
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("your", "my");     
+        map.put("my", "your");
+        map.put("I", "you");
+        map.put("you", "I");
+        map.put("am", "are");
+        map.put("are","am");
+        map.put("me", "you");
+        map.put("mine", "yours");
+        map.put("yours", "mine");
+        map.put(".", "?");
 
-    //? How to make prior replacements not be overrridden by later ones
-    // Makeshift dictionary to replace required words
-    HashMap<String, String> map = new HashMap<String, String>();
-    map.put("your", "my");
-    map.put("my", "your");
-    map.put("I", "you");
-    map.put("you", "I");
-    map.put("am", "are");
-    map.put("me", "you");
-    map.put("mine", "yours");
-    map.put("yours", "mine");
-    map.put(".", "?");
+        // Creating canned responses
+        ArrayList<String> canned_responses = new ArrayList<String>();
+        canned_responses.add("Wow.");       //adding each response to array
+        canned_responses.add("Jeez.");
+        canned_responses.add("If you say so...");
+        canned_responses.add("That's kinda rude.");
+        canned_responses.add("I'm trying to understaind where you're coming from.");
+        canned_responses.add("Okay...");
+        canned_responses.add("No no I hear you.");
+        canned_responses.add("It sounds like you're not taking responsibility for your part in it.");
+        canned_responses.add("Anything else?");
 
-    // Creating canned messages
-    ArrayList<String> canned_responses = new ArrayList<String>();
-    canned_responses.add("Wow.");
-    canned_responses.add("Jeez.");
-    canned_responses.add("If you say so...");
-    canned_responses.add("That's kinda rude.");
-    canned_responses.add("I'm trying to understaind where you're coming from.");
-    canned_responses.add("Okay...");
-    canned_responses.add("No no I hear you.");
-    canned_responses.add("It sounds like you're not taking responsibility for your part in it.");
-    canned_responses.add("Anything else?");
+        // Generate random numbers to choose a corresponding canned response
+        Random rand = new Random();     //initializing random class
+        int upperbound = canned_responses.size();       //sets range of values to choose from
+        int int_random = rand.nextInt(upperbound);      //variable represents one randomly generated integer
+        //credit the website for this code
 
-    // Generate random numbers to choose a corresponding canned response
-    Random rand = new Random(); 
-    int upperbound = canned_responses.size();
-    int int_random = rand.nextInt(upperbound); 
-    //System.out.println("Random integer value from 0 to" + (upperbound-1) + " : "+ int_random);
-    //credit the website for this code
+        Scanner input = new Scanner(System.in);
 
-    // Starting conversation here. How many times to ask for input
-    System.out.println("How many times are we going to go over this?");
-    Scanner input = new Scanner(System.in);
-    int n = input.nextInt();
-    input.nextLine(); //to clear buffer
+        //Ask how many rounds
+        System.out.println("How many times are we going to go over this?");
+        int n = input.nextInt();
+        input.nextLine();       //clear buffer of enter key above
 
-    // If answer is 0, print a goodbye message
-    if (n == 0) {
-      System.out.println("That's what I thought. I'm done.");
-      input.close();
-    }
-    
-    // If answer is not 0, begins chat, reducing value of n after each exchange
-    else {
-      //System.out.println("Okay, what do you have to say for yourself?");
-      //String input1 = input.nextLine(); //for transcript will I be able to refer to this variable in this method?
-      //System.out.println("Really? You're saying " + input1 +"?");
-      //n = n-1;
-
-      // If they didn't input 0, the for loop will provide a tailored message based on mirror words
-      // If no mirror words detected, respond with randomized canned message
-      for (int i=0; i<n; i++){
-        String response = input.nextLine();
-        String[] words = response.split(" ");
-
-        for (int h=0; h <words.length; h++) { //? what are the normal for loop variables to use
-          if(map.containsKey(words[h])) {
-            response = response.replace(words[h], (String)map.get(words[h]));
-          }
-
-      //System.out.println(i + " " + n);
-      //System.out.println("what else?"); 
-      int_random = rand.nextInt(upperbound);
-      System.out.println(canned_responses.get(int_random));
-      //input.nextLine(); //save response as split --find throuhg string documentation--
-      //String input3 = input.next();
-      //String[] words=input2.split(" ");
-      //System.out.println(input2);
-      //string user  String[] words=userInput.split(" ") ...gives an array of individual words...
-      }
-
-      // Once n=0, goodbye message prints
-      System.out.println("That's it. I'm done having this conversation.");
-      input.close();
-      
-    
-      /* set up dictionary... mhmm... okay and? I see what you're saying 
-         I don't know about that
-       * may need a separate class and thus file for that.
-       * generate a random number used as a key for a dictionary
-       * print dictionary term
-       * ask for input again
-       * LEVEL 3 wants to save transcript, so this may require
-       * putting the input and what we've said into an array?
-       * close input
-       */
-  
-    }
-
-    
-  }
-
-
-}
+        if(n == 0){     //if user didn't want to talk 
+            System.out.println("That's what I thought. I didn't even want to talk to you anyway");
+            input.close();
+        }
+        else {
+            System.out.println("So what do you have to say for yourself?");
+            for(int i = 0; i < n; i++){
+                String typed = input.nextLine();
+                String typed_words[] = typed.split(" ");
+                    //System.out.println(Arrays.toString(typed_words));
+                    Boolean mirrorflag = false;
+                    for(int j = 0; j < typed_words.length; j++){
+                        if(map.containsKey(typed_words[j])){
+                            mirrorflag = true;
+                            typed_words[j]= map.get(typed_words[j]);
+                            //typed_words.replace(typed_words[j], (String) map.get(typed_words[j])); 
+                            System.out.println(Arrays.toString(typed_words));
+                        }
+                    }
+                    if(mirrorflag){
+                        //going to print string from typed_words[] array
+                    }
+                    if(!mirrorflag){
+                        int_random = rand.nextInt(upperbound);
+                        System.out.println(canned_responses.get(int_random)); 
+                    }
+            }
+            System.out.println("That's it. I'm done having this conversation");
+            input.close();
+        } 
+    }   
 }
